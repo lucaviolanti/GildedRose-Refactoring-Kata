caa7639489d03cb623dac2fad069d5f891525bbe
Luca Violanti <lv185028@ncr.com>
2021-03-17 16:49:30 +0000

    Refactor section for the rest using "Lift Conditional" pattern

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -46,23 +46,16 @@ class GildedRose(val items: Array[Item]) {
         }
         else {
           if (item.quality > 0) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-              item.quality = item.quality - 1
-            }
+            item.quality = item.quality - 1
           }
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1
-          }
+          item.sellIn = item.sellIn - 1
           if (item.sellIn < 0) {
             if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1
-              }
+              item.quality = item.quality - 1
             }
           }
         }
       }
     }
   }
-
 }
