e4ccdb3aec6d01c2cec3787d27d380218537826a
Luca Violanti <lv185028@ncr.com>
2021-03-17 16:43:55 +0000

    Refactor section for Backstage Passes using "Lift Conditional" pattern

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -23,45 +23,43 @@ class GildedRose(val items: Array[Item]) {
       }
     }
     else {
-      if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (item.quality > 0) {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
+      if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
-
           if (item.sellIn < 11) {
             if (item.quality < 50) {
               item.quality = item.quality + 1
             }
           }
-
           if (item.sellIn < 6) {
             if (item.quality < 50) {
               item.quality = item.quality + 1
             }
           }
         }
-      }
-
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
         item.sellIn = item.sellIn - 1
+        if (item.sellIn < 0) {
+          item.quality = 0
+        }
       }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      else {
+        if (item.quality > 0) {
+          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality = item.quality - 1
+          }
+        }
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+          item.sellIn = item.sellIn - 1
+        }
+        if (item.sellIn < 0) {
           if (item.quality > 0) {
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
               item.quality = item.quality - 1
             }
           }
-        } else {
-          item.quality = 0
         }
       }
     }
   }
+
 }
