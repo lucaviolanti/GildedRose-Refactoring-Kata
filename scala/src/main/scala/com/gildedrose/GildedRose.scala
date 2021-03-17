6e5d54a814410062e9c11e7e96949663d50d50c3
lucaviolanti <luca.violanti@tutanota.com>
2021-03-17 22:06:48 +0000

    Simplify update logic

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -4,23 +4,20 @@ class GildedRose(val items: Array[Item]) {
   def updateQuality() {
     items.foreach {
       case brie@AgedBrie(_, _) => {
-        if (brie.quality < 50) {
-          brie.quality = brie.quality + 1
-        }
         brie.sellIn = brie.sellIn - 1
-        if (brie.sellIn < 0 && brie.quality < 50) {
-          brie.quality = brie.quality + 1
+        if (brie.quality < 50) {
+          brie.quality += (if (brie.sellIn < 0) 2 else 1)
         }
       }
 
       case pass@BackstagePass(_, _) => {
         if (pass.quality < 50) {
-          pass.quality = pass.quality + 1
+          pass.quality += 1
           if (pass.sellIn < 11 && pass.quality < 50) {
-            pass.quality = pass.quality + 1
+            pass.quality += 1
           }
           if (pass.sellIn < 6 && pass.quality < 50) {
-            pass.quality = pass.quality + 1
+            pass.quality += 1
           }
         }
         pass.sellIn = pass.sellIn - 1
@@ -34,11 +31,11 @@ class GildedRose(val items: Array[Item]) {
 
       case item@_ => {
         if (item.quality > 0) {
-          item.quality = item.quality - 1
+          item.quality -= 1
         }
-        item.sellIn = item.sellIn - 1
+        item.sellIn -= 1
         if (item.sellIn < 0 && item.quality > 0) {
-          item.quality = item.quality - 1
+          item.quality -= 1
         }
       }
     }
